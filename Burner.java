b8f014d7d6aebe9526c7d7bdb1b6020a9d88cea6
BuildTools <unconfigured@null.spigotmc.org>
2026-02-02 18:48:38 -0700

    local vars added

--- Burner.java
+++ Burner.java
@@ -1,4 +1,5 @@
 //Author Jiah and Chase
+
 public class Burner {
 	
 	private enum Temperature {
@@ -7,4 +8,8 @@ public class Burner {
 		WARM,
 		COLD
 	}
+	
+	private Setting mySetting;
+	private int timer;
+	public final static int TIME_DURATION = 2;
 }
